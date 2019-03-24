2a882394c6194640fe9256b493779cf8bff02b33
Wei-Fan <zwf_jaccount@sjtu.edu.cn>
2019-03-24 01:58:14 -0400

    finish display_area

--- cmake-build-debug/catkin_generated/setup_cached.sh
+++ cmake-build-debug/catkin_generated/setup_cached.sh
@@ -8,5 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/wade/catkin_ws/src/restart/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/wade/catkin_ws/src/restart/cmake-build-debug/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/wade/catkin_ws/src/restart/cmake-build-debug/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/wade/catkin_ws/src/restart/cmake-build-debug/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/wade/catkin_ws/src/restart/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/wade/catkin_ws/src/restart:$ROS_PACKAGE_PATH"
--- src/swarm_commander.cpp
+++ src/swarm_commander.cpp
@@ -1,12 +1,30 @@
 #include <ros/ros.h>
 #include <iostream>
 #include <string>
+#include <cv_bridge/cv_bridge.h>
+#include "Eigen/Eigen/Eigen"
+#include "Eigen/Eigen/Geometry"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <vector>
 //#include <geometry_msgs/Pose.h>
 //#include <geometry_msgs/Twist.h>
 
 #define DEFAULT_RATE 50
 
 using namespace std;
+using namespace cv;
+using namespace Eigen;
+
+int agent_num = 0;
+int target_num = 0;
+bool isAgentDone = false;
+vector<float> x_init;
+vector<float> y_init;
+vector<float> x_target;
+vector<float> y_target;
 
 class SwarmCommander
 {
@@ -16,11 +34,10 @@ private:
     ros::NodeHandle global;
     ros::NodeHandle local;
 
-    /*Publish the setpoint*/
-//    ros::Publisher cmd_sp_pub;
-
-    /*Publish the velocity command*/
-//    ros::Publisher cmd_vel_pub;
+    Mat src = Mat(Size(1000,1000), CV_8UC3, Scalar(0));
+    Mat assignment;
+    int gird_size = 40;
+    int area_size = 800;
 
 public:
     SwarmCommander(){
@@ -29,12 +46,73 @@ public:
     }
 
     void run(){
-        ros::Rate r(DEFAULT_RATE);
-        while(ros::ok()){
-            /*update curr_pos and cmd_sp. Publish them.*/
+        /*
+         * initial phase
+         * Area size: 40*40
+         * */
+        display_area();
+        /*
+         * divide area
+         * */
+        divide_area();
+
+        /*
+         * STC planning
+         * */
+        path_planning();
+
+    }
+
+    static void onMouse(int event, int x, int y, int, void* userInput)
+    {
+        if (event != EVENT_LBUTTONDOWN) return;
+        //printf("###########onMouse x : %d\n", x);
+        //printf("###########onMouse y : %d\n", y);
+        int x_world = x - 500;
+        int y_world = 500 - y;
+        Mat *img = (Mat*)userInput;
+
+        circle(*img, Point(x, y), 6, Scalar(255, 0, 255), 2);
+        imshow("coverage_monitor", *img);
+        x_init.push_back(x);//x_world);
+        y_init.push_back(y);//y_world);
+        agent_num++;
+
+
+    }
+
+    void display_area(){
+        /*draw an empty area*/
+        namedWindow("coverage_monitor");
+        Point p1 = Point(50,50);
+        Point p2 = Point(area_size+50,area_size+50);
+        rectangle(src, p1, p2, CV_RGB(245, 245, 245), -1);
+//        imshow("coverage_monitor", src);
+
+        /*draw grids*/
+        int stepSize = 800/gird_size;
+        for (int i = stepSize; i < area_size; i += stepSize) {
+            line(src, Point(50,50+i),Point(area_size+50,50+i),Scalar(0,0,0));
         }
-        ros::spinOnce();
-        r.sleep();
+
+        for (int i = stepSize; i < area_size; i += stepSize) {
+            line(src, Point(50+i,50),Point(50+i,area_size+50),Scalar(0,0,0));
+        }
+        imshow("coverage_monitor", src);
+
+        setMouseCallback("coverage_monitor", onMouse, &src);
+        waitKey();
+        printf("robot number : %d\n", agent_num);
+
+        destroyWindow("coverage_monitor");
+    }
+
+    bool divide_area(){
+
+    }
+
+    void path_planning(){
+
     }
 };
 
