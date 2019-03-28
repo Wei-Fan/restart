906223f9461a25d9657fb1c8e46e7c62bc4d5980
Wei-Fan <zwf_jaccount@sjtu.edu.cn>
2019-03-27 23:09:41 -0400

    finish check every grid

--- src/swarm_commander.cpp
+++ src/swarm_commander.cpp
@@ -15,6 +15,7 @@
 
 #define DEFAULT_RATE 50
 #define GRID_SIZE 40
+#define CORE_SIZE 20 //CORE_SIZE = GRID_SIZE / 2
 #define AREA_SIZE 800
 
 using namespace std;
@@ -33,7 +34,11 @@ private:
 
     Mat board = Mat(Size(900,900), CV_8UC3, Scalar(0));
 
-    Matrix<int,GRID_SIZE,GRID_SIZE> K;
+    Matrix<int,CORE_SIZE,CORE_SIZE> K;
+//    Matrix<int,GRID_SIZE,GRID_SIZE> E; How to define a 3d matrix?
+//    MatrixXi E;
+    Matrix<double,Dynamic,CORE_SIZE> C;
+    Matrix<double,Dynamic,1> m;
     vector<int> robot_grid_x;
     vector<int> robot_grid_y;
 
@@ -52,7 +57,6 @@ public:
          * initial phase
          * Area size: 40*40
          * */
-//        ROS_INFO("debug 1");
         display_area();
         
         /*initialize robot positions by clicking (testing code)*/
@@ -60,18 +64,29 @@ public:
         imshow("monitor",board);
         setMouseCallback("monitor", onMouse, &board);
         waitKey();
-        ROS_INFO("robot number : %d", x_init.size());
+        ROS_INFO("robot number : %d", robot_number);
         destroyWindow("monitor");
         for (int i = 0; i < x_init.size(); ++i) {
-            robot_grid_x.push_back((int)((x_init[i] - 50)*GRID_SIZE/AREA_SIZE));
-            robot_grid_y.push_back((int)((y_init[i] - 50)*GRID_SIZE/AREA_SIZE));
+            robot_grid_x.push_back((x_init[i] - 50)*CORE_SIZE/AREA_SIZE);
+            robot_grid_y.push_back((y_init[i] - 50)*CORE_SIZE/AREA_SIZE);
             ROS_INFO("robot : %d,%d",robot_grid_x[i],robot_grid_y[i]);
         }
+
+        /*these codes need to be moved if the robot number comes from elsewhere*/
+//        E = MatrixXi::Zero(robot_number*CORE_SIZE,CORE_SIZE);
+        // To access : MatrixXi Ek = E.block<CORE_SIZE,CORE_SIZE>(k*CORE_SIZE,0);
+        ROS_INFO("debug 1");
+        C.resize(robot_number*CORE_SIZE,CORE_SIZE);
+        C.setOnes();
+        m.resize(robot_number,1);
+        m.setOnes(); //size : (robot_number,1)
 
         /*
          * divide area
          * */
-//        divide_area();
+
+        ROS_INFO("debug 2");
+        divide_area();
 
         /*
          * STC planning
@@ -102,12 +117,12 @@ public:
 //        ROS_INFO("draw grids complete");
 
         /*draw result*/
-        for (int i = 0; i < GRID_SIZE; i += 1) { //cols
-            for (int j = 0; j < GRID_SIZE; j += 1) { //rows
+        for (int i = 0; i < CORE_SIZE; i += 1) { //cols
+            for (int j = 0; j < CORE_SIZE; j += 1) { //rows
                 if (K(j,i)==0)
                     continue;
-                Point t1 = Point(j*stepSize+50,i*stepSize+50);
-                Point t2 = Point((j+1)*stepSize+50,(i+1)*stepSize+50);
+                Point t1 = Point(j*stepSize*2+50,i*stepSize*2+50);
+                Point t2 = Point((j+1)*stepSize*2+50,(i+1)*stepSize*2+50);
                 rectangle(board, t1, t2, CV_RGB(0,0,255), -1);
             }
         }
@@ -141,7 +156,40 @@ public:
     }
 
     bool divide_area(){
+        /*Generate the first matrix*/
 
+        /*main loop*/
+        bool stop = false;
+        int iteration_count = 0;
+        while (!stop&&ros::ok())
+        {
+            iteration_count++;
+            ArrayXi S = ArrayXi::Zero(robot_number); // record grid number for each robot
+//            ROS_INFO("S size : %d, %d and %d", S.rows(), S.cols(),S(2));
+            int count = 0;
+
+            /*check every grid*/
+//            ROS_INFO("E size : %d, %d",E.rows(),E.cols());
+            for (int i = 0; i < CORE_SIZE; ++i) { //cols
+                for (int j = 0; j < CORE_SIZE; ++j) { //rows
+                    double tmp = -1;
+                    for (int k = 0; k < robot_number; ++k) {
+//                        MatrixXi E_t = E.block<CORE_SIZE,CORE_SIZE>(k*CORE_SIZE,0);
+//                        MatrixXi C_t = C.block<CORE_SIZE,CORE_SIZE>(k*CORE_SIZE,0);
+                        double Ekji = C(j+k*CORE_SIZE,i) * m(k,1) * sqrt((i-robot_grid_x[k])*(i-robot_grid_x[k])+(j-robot_grid_y[k])*(j-robot_grid_y[k]));
+//                        double E_t = ;
+                        if (tmp < 0 || tmp > Ekji)
+                        {
+                            tmp = Ekji;
+                            K(j,i) = k;
+                        }
+                    }
+                    S(K(j,i),1)++;
+                }
+            }
+//            cout << S <<  endl;
+//            stop = true;
+        }
     }
 
     void path_planning(){
